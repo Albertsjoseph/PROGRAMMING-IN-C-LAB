09cc0f8a270f4df6f6890cd67ace3b1855e6d31e
Albertsjoseph <96837954+Albertsjoseph@users.noreply.github.com>
2022-03-18 15:07:58 +0530

    Add files via upload

---
+++ LIMITOFT.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void create();
+void main()
+{
+	create();
+
+}
+void create()
+{
+	int a[100],n,i=0;
+	printf("Enter the limit of the array");
+	scanf("%d",&n);
+	printf("Enter the elements:");
+	while(i<n)
+	{
+		scanf("%d",&a[i]);
+		i++;
+	}
+	printf("ELEMENTS ARE:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+getch();
+}
---
+++ SUMOFTHE.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void create();
+void main()
+{
+	create();
+}
+void create()
+{
+	int a[100],n,i=0,sum=0;
+	printf("Enter the limit of the array");
+	scanf("%d",&n);
+	printf("Enter the elements:");
+	while(i<n)
+	{
+		scanf("%d",&a[i]);
+		sum+=a[i];
+		i++;
+	}
+	printf("SUM OF THE ELEMENTS=%d",sum);
+getch();
+}
